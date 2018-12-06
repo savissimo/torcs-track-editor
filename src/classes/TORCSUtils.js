ed46441cbd8cffb93c36b19cd4366432846e9f53
savissimo <simone.saviolo@gmail.com>
2018-12-06 16:47:26 +0100

    Crash fix for missing attributes

--- src/classes/TORCSUtils.js
+++ src/classes/TORCSUtils.js
@@ -1,5 +1,6 @@
 export default class TORCSUtils {
 	static getStringAttribute(i_xmlNode, i_key) {
+		if (!i_xmlNode) { return ''; }
 		let attribute = i_xmlNode.querySelector('attstr[name="' + i_key + '"]');
 		if (attribute && attribute.parentElement === i_xmlNode) {
 			return attribute.getAttribute('val') + '';
@@ -8,6 +9,7 @@ export default class TORCSUtils {
 	}
 
 	static getNumericAttribute(i_xmlNode, i_key) {
+		if (!i_xmlNode) { return 0; }
 		let attribute = i_xmlNode.querySelector('attnum[name="' + i_key + '"]');
 		if (attribute && attribute.parentElement === i_xmlNode) {
 			return Number(attribute.getAttribute('val'));
